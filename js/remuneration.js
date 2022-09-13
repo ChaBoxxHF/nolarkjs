620202f9f3b734c95aa14ee0333e2b473e1c84d5
Pascal 'ChaBoxxHF' CROCHARD <73274074+ChaBoxxHF@users.noreply.github.com>
2022-09-13 10:51:40 +0200

    ajout de la feature simulateur de rémunération
    WIP

---
+++ js/remuneration.js
@@ -0,0 +1,15 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+
+const fixe = 1100;
+const cinq = 1100+(1100*0.03);
+const dix = 1100+ (1100*0.06); //on pourrait aussi faire une variable ancienneté
+
+let nbAncien = parseInt(window.document.querySelector("#num_ancien").value);
+
+function remuneration(){
+    
+}
