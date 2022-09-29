1721e2ba3767030eaa56bfb61cac8c5888065de1
Pascal 'ChaBoxxHF' CROCHARD <73274074+ChaBoxxHF@users.noreply.github.com>
2022-09-29 09:41:49 +0200

    recuperation du js de jeremy + ajouts

---
+++ js/remunerationv2.js
@@ -0,0 +1,74 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+
+    //var decalaration
+const salairefixe = 1100, prixS = 140, prixM = 180, prixXS= 350;
+
+window.addEventListener("load", function() {
+    window.document.querySelector("#btn_envoyer").addEventListener("click", simulSalaire);
+    });
+   
+    
+    
+    
+    
+
+    function simulSalaire(){
+        let nbventeS = document.querySelector("#num_S20").value;
+        let nbventeM = document.querySelector("#num_Multitec").value;
+        let nbventeXS = document.querySelector("#num_XSpirit").value;
+        
+        let salaire = PrimeAnciennete(salairefixe) + calccomS(nbventeS) + calccomM(nbventeM) + calccomXS(nbventeXS);
+        console.log("salaire :" + salaire);
+        document.querySelector("#resultSimu").innerHTML = salaire;
+        return (salaire); 
+    }
+    
+
+
+
+function calccomS(nbventeS){
+    let comS = 0;
+    comS = (prixS * nbventeS)/50;
+    return comS;
+}
+    
+function calccomM(nbventeM){
+    let comM = (prixM*nbventeM)/25;
+    if(nbventeM>20){
+        comM = comM + (prixM*(nbventeM - 20))/(100/6);
+        if(nbventeM > 50){
+            comM = comM + (prixM*(nbventeM - 50))/(10);
+        }
+    }
+    return comM;
+}
+function calccomXS(nbventeXS){
+    let comXS = 0;
+    if(nbventeXS > 50){
+        comXS = (prixXS*(nbventeXS-50))/(100/6);
+    }
+    return comXS;
+}
+
+function PrimeAnciennete(fixe){
+        let salaireAncien;
+        const ancienSup10=1.06;
+        const ancienInf10Sup5=1.03;
+        switch(parseInt(document.querySelector("#lst_nbAncien").value)){
+            case 1:
+                salaireAncien = fixe;
+                break;
+            case 2:
+                salaireAncien=fixe * ancienInf10Sup5;
+                break;
+            case 3:
+                salaireAncien=fixe*ancienSup10;
+                break;
+        }
+        return salaireAncien;
+    }
+
