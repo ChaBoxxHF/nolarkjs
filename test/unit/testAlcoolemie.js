69648349c2cb86e0854c51929676e64f22024677
Pascal 'ChaBoxxHF' CROCHARD <73274074+ChaBoxxHF@users.noreply.github.com>
2022-10-06 11:37:32 +0200

    debut du tp test unitaire

---
+++ test/unit/testAlcoolemie.js
@@ -0,0 +1,16 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+
+/**
+ * Tests unitaires du projet Alcoolémie
+ * 
+ */
+
+MesTestsUnitaires = TestCase('AlccolemieTest');
+MesTestsUnitaires.prototype.testsGetAlcoolPur = function () {
+    assertEquals('0 verre', 0, getAlcoolPur(0));
+    assertEquals('1 verre', 10, getAlcoolPur(1));
+};
